4d14a6bc22e2eb4b818631c599331b605d00e0fa
hbeffge <65234543+hbeffge@users.noreply.github.com>
2020-07-06 12:09:55 +0200

    Update sni-tester.sh

--- sni-tester.sh
+++ sni-tester.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # set default settings
-kubeconfig=$KUBECONFIG
+kubeconfig=~/.kube/config
 repeat=1
 counter=1
 setupistio=0
@@ -11,7 +11,7 @@ workdir=/tmp/sni-tester
 usage()
 {
     echo -e "\tusage: ./sni-tester.sh [-h]"
-    echo -e "\tUse -k or --kubeconfig to sepcify kubeconfig file location. Default is env var KUBECONFIG = $KUBECONFIG"
+    echo -e "\tUse -k or --kubeconfig to sepcify kubeconfig file location. Default is env var KUBECONFIG = ~/.kube/config"
     echo -e "\tUse -r or --repeat to sepcify how many sni configs should be generated. Default is 1"
     echo -e "\tUse -c or --counter to specify the last position of the last created sni config. Default is 1"
 }
@@ -20,7 +20,7 @@ setup_istio()
 {
     curl -L https://istio.io/downloadIstio | sh -
     export PATH="$PATH:/root/istio-1.6.4/bin"
-    istioctl install --set profile=default --set meshConfig.accessLogFile="/dev/stdout"
+    istioctl --kubeconfig $kubeconfig install --set profile=default --set meshConfig.accessLogFile="/dev/stdout"
 }
 
 
@@ -71,19 +71,50 @@ then
     mkdir $workdir
 fi
 
-if [ ! -f "$kubectl delete ns customer/example.com.crt" ]; then
+if [ ! -f "$workdir/example.com.crt" ]; then
     openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -subj '/O=example Inc./CN=example.com' -keyout $workdir/example.com.key -out $workdir/example.com.crt
 fi
+
+# create service entry and destionation used by all
+
+cat << EOF | kubectl --kubeconfig $kubeconfig apply -f -
+---
+apiVersion: networking.istio.io/v1beta1
+kind: ServiceEntry
+metadata:
+  name: external-httpbin
+  namespace: istio-system
+spec:
+  hosts:
+  - httpbin.org
+  location: MESH_EXTERNAL
+  ports:
+  - number: 443
+    name: https-httpbin
+    protocol: HTTPS
+  resolution: DNS
+---
+apiVersion: networking.istio.io/v1beta1
+kind: DestinationRule
+metadata:
+  name: external-httpbin
+  namespace: istio-system
+spec:
+  host: "httpbin.org"
+  trafficPolicy:
+    tls:
+      mode: SIMPLE
+EOF
 
 while [ "$repeat" -gt 0 ]
 do
     # create certificate
     openssl req -out $workdir/$counter.example.com.csr -newkey rsa:2048 -nodes -keyout $workdir/$counter.example.com.key -subj "/CN=$counter.example.com/O=example organization"
     openssl x509 -req -days 365 -CA $workdir/example.com.crt -CAkey $workdir/example.com.key -set_serial 0 -in $workdir/$counter.example.com.csr -out $workdir/$counter.example.com.crt
-    kubectl create -n istio-system secret tls credential-$counter --key=$workdir/$counter.example.com.key --cert=$workdir/$counter.example.com.crt
+    kubectl --kubeconfig $kubeconfig create -n istio-system secret tls credential-$counter --key=$workdir/$counter.example.com.key --cert=$workdir/$counter.example.com.crt
     
     # create gateway
-    cat << EOF | kubectl apply -f -
+    cat << EOF | kubectl --kubeconfig $kubeconfig apply -f -
 apiVersion: networking.istio.io/v1beta1
 kind: Gateway
 metadata:
@@ -121,31 +152,6 @@ spec:
           number: 443
         host: httpbin.org
 ---
-apiVersion: networking.istio.io/v1beta1
-kind: ServiceEntry
-metadata:
-  name: external-httpbin
-  namespace: istio-system
-spec:
-  hosts:
-  - httpbin.org
-  location: MESH_EXTERNAL
-  ports:
-  - number: 443
-    name: https-httpbin
-    protocol: HTTPS
-  resolution: DNS
----
-apiVersion: networking.istio.io/v1beta1
-kind: DestinationRule
-metadata:
-  name: external-httpbin
-  namespace: istio-system
-spec:
-  host: "httpbin.org"
-  trafficPolicy:
-    tls:
-      mode: SIMPLE
 EOF
     (( counter = counter + 1 ))
     (( repeat = repeat - 1 ))
